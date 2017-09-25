ad447b2f3bafe40801ecd44ab83db7ae9b9c0bd7
GuillaumeCisco <guillaumecisco@gmail.com>
2017-09-25 13:12:01 +0200

    Update code for supporting dom stream

--- src/server/render.js
+++ src/server/render.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/server';
+import ReactDOMStream from 'react-dom-stream/server';
+import LRURenderCache from 'react-dom-stream/lru-render-cache';
 import {JssProvider, SheetsRegistry} from 'react-jss';
 import {create} from 'jss';
 import preset from 'jss-preset-default';
@@ -25,6 +27,8 @@ const sheetsRegistry = new SheetsRegistry();
 const jss = create(preset());
 jss.options.createGenerateClassName = createGenerateClassName;
 
+const cache = LRURenderCache({max: 64 * 1024 * 1024});
+
 const createApp = (App, store) =>
     (<Provider store={store}>
         <JssProvider registry={sheetsRegistry} jss={jss}>
@@ -35,24 +39,8 @@ const createApp = (App, store) =>
     </Provider>);
 
 
-export default ({clientStats}) => async (req, res, next) => {
-    const store = await configureStore(req, res);
-    if (!store) return; // no store means redirect was already served
-
-    const app = createApp(App, store);
-    const {html, ids, css} = extractCritical(ReactDOM.renderToString(app));
-
-    // Grab the CSS from our sheetsRegistry.
-    const materialUiCss = sheetsRegistry.toString();
-    const stateJson = JSON.stringify(store.getState());
-    const chunkNames = flushChunkNames();
-    const {js, styles, cssHash} = flushChunks(clientStats, {chunkNames});
-
-    console.log('REQUESTED PATH:', req.path);
-    console.log('CHUNK NAMES', chunkNames);
-
-    return res.send(
-        `<!doctype html>
+const earlyChunk = (styles, materialUiCss, css, stateJson, ids) => `
+    <!doctype html>
       <html>
         <head>
           <meta charset="utf-8">
@@ -63,19 +51,56 @@ export default ({clientStats}) => async (req, res, next) => {
           <meta name="description" content="${META_DESCRIPTION}"/>
           <meta name="keywords" content="${META_KEYWORDS}" />
           ${styles}
-          <style type="text/css">${css}</style>
           <style id="jss-server-side">${materialUiCss}</style>
+          <style type="text/css">${css}</style>
         </head>
-        <body>
+      <body>
           <script>window.REDUX_STATE = ${stateJson}</script>
-          <script>${`window.EMOTION_IDS = new Array("${ids}")`}</script>
-          <div id="root">${process.env.NODE_ENV === 'production' ? html : `<div>${html}</div>`}</div>
-          ${cssHash}          
-          <script type="text/javascript" src="/reactVendors.js"></script>
-          <script type="text/javascript" src="/vendors.js"></script>
-          ${js}          
+          <script>window.EMOTION_IDS = new Array("${ids}")</script>
+          <div id="root">${process.env.NODE_ENV !== 'production' ? '<div>' : ''}`,
+    lateChunk = (cssHash, js) => `
+          ${process.env.NODE_ENV !== 'production' ? '</div>' : ''}
+          </div>
+          ${cssHash}       
+          <script type='text/javascript' src='/reactVendors.js'></script>
+          <script type='text/javascript' src='/vendors.js'></script>
+          ${js}
           ${serviceWorker}
         </body>
-      </html>`,
-    );
+    </html>
+  `;
+
+export default ({clientStats}) => async (req, res, next) => {
+    const store = await configureStore(req, res);
+    if (!store) return; // no store means redirect was already served
+
+    // Grab the CSS from our sheetsRegistry.
+    const materialUiCss = sheetsRegistry.toString();
+    const stateJson = JSON.stringify(store.getState());
+    const chunkNames = flushChunkNames();
+    const {js, styles, cssHash} = flushChunks(clientStats, {chunkNames});
+
+    const app = createApp(App, store);
+
+    // TODO Remove when extractCritical can run with stream
+    const Sync = extractCritical(ReactDOM.renderToString(app));
+
+    const {html, ids, css} = extractCritical(ReactDOMStream.renderToString(app, {cache}));
+
+    res.set('Content-Type', 'text/html');
+    // flush the head with css & js resource tags first so the download starts immediately
+    const early = earlyChunk(styles, materialUiCss, Sync.css, stateJson, ids);
+    res.write(early);
+    res.flush();
+
+    console.log('REQUESTED PATH:', req.path);
+    console.log('CHUNK NAMES', chunkNames);
+
+    const stream = html;
+    stream.pipe(res, {end: false});
+    stream.on('end', () => {
+        const late = lateChunk(cssHash, js);
+        res.write(late);
+        res.end();
+    });
 };
